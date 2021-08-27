7113f40886292d84637a6acbcca44e741a79c5d8
yoojm4718 <yoojm4718@outlook.com>
2021-08-27 13:42:12 +0900

    Configured AWS and Heroku

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,9 +1,23 @@
 import multer from "multer";
+import multers3 from "multer-s3";
+import aws from "aws-sdk";
+
+const isHeroku = process.env.NODE_ENV === "production";
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.loggedInUser = req.session.user;
   res.locals.siteName = "Wetube";
+  res.locals.staticURL = isHeroku
+    ? "https://wetube-yoojm4718.s3.us-east-2.amazonaws.com/"
+    : "/";
   next();
 };
 
@@ -25,11 +39,25 @@ export const publicOnlyMiddleWare = (req, res, next) => {
   }
 };
 
+const avatarUploader = multers3({
+  s3: s3,
+  bucket: "wetube-yoojm4718/uploads/avatar",
+  acl: "public-read",
+});
+
+const videoUploader = multers3({
+  s3: s3,
+  bucket: "wetube-yoojm4718/uploads/video",
+  acl: "public-read",
+});
+
 export const avatarUpload = multer({
   dest: "uploads/avatar",
   limits: { fileSize: 3000000 },
+  storage: isHeroku ? avatarUploader : undefined,
 });
 export const videoUpload = multer({
   dest: "uploads/video",
   limits: { fileSize: 10000000 },
+  storage: isHeroku ? videoUploader : undefined,
 });
